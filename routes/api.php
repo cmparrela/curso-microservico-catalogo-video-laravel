818c4fdbcbd9a7dca3e25aa99f361a17e77b53e9
Cristopher Parrela <cristopherparrela@finchsolucoes.com.br>
2021-04-14 03:49:31 +0000

    Melhoria nas rotas

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
- */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
-Route::resource('genres', 'GenreController', ['except' => ['create', 'edit']]);
+$exceptCreateAndEdit = [
+    'except' => ['create', 'edit'],
+];
+Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
+Route::resource('genres', 'GenreController', $exceptCreateAndEdit);
